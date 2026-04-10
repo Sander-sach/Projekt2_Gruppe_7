76ce626dd4120bfec83c9ebf65fd468d91941c5a
Sander <sander.o.christensen@gmail.com>
2026-04-10 11:37:58 +0200

    First Scaffold commit

---
+++ src/main/java/com/example/projekt2_gruppe7/Projekt2Gruppe7Application.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2Gruppe7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2Gruppe7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt2_gruppe7/Projekt2Gruppe7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt2_gruppe7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt2Gruppe7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
