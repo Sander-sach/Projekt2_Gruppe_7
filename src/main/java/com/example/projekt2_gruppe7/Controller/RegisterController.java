333e755335b79e883affdf02b90811daa6bd12f0
Sander <sander.o.christensen@gmail.com>
2026-04-13 16:25:13 +0200

    Feature/US-1 Fixed SDK misconfig for groupe

--- src/main/java/com/example/projekt2_gruppe7/Controller/RegisterController.java
+++ src/main/java/com/example/projekt2_gruppe7/Controller/RegisterController.java
@@ -1,5 +1,6 @@
 package com.example.projekt2_gruppe7.Controller;
 
+import com.example.projekt2_gruppe7.service.UserService;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,19 +17,19 @@ this.userService = userService;
 }
 @GetMapping("/register")
     public String showRegister(Model model){
-    model.addAttribute("userDTO", new UserDTO());
+    model.addAttribute("userDTO");
     return "register";
 
 }
     @PostMapping("/register")
-    public String handleRegister(@ModelAttribute("userDTO") UserDTO userDTO,
+    public String handleRegister(@ModelAttribute("userDTO")
                                  BindingResult result,
                                  Model model) {
     if (result.hasErrors()){
         return "register";
     }
     try {
-        userService.registerUser(userDTO);
+        userService.registerUser();
         return "redirect:/login";
     }catch (Exception e){
         model.addAttribute("errorMessage",e.getMessage());
