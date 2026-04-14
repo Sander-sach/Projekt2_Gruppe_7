274eb5ec070460b24177a1b262b7ccae28154e2a
Sander <sander.o.christensen@gmail.com>
2026-04-14 16:54:19 +0200

    Feature/US-4,US-5 wish and wishlist models done and updated DB along with SQl script

--- src/main/java/com/example/projekt2_gruppe7/model/Wish.java
+++ src/main/java/com/example/projekt2_gruppe7/model/Wish.java
@@ -1,4 +1,61 @@
 package com.example.projekt2_gruppe7.model;
 
 public class Wish {
+    private Long id;
+    private Long wishListId;
+    private String itemName;
+    private String description;
+    private Double price;
+    private String itemURL;
+
+    public Wish(Long wishListId, String itemName, String description, Double price, String itemURL) {
+        this.wishListId = wishListId;
+        this.itemName = itemName;
+        this.description = description;
+        this.price = price;
+        this.itemURL = itemURL;
+    }
+
+    public Wish() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getWishListId() {
+        return wishListId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getItemURL() {
+        return itemURL;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setItemURL(String itemURL) {
+        this.itemURL = itemURL;
+    }
 }
--- src/main/java/com/example/projekt2_gruppe7/model/WishList.java
+++ src/main/java/com/example/projekt2_gruppe7/model/WishList.java
@@ -1,4 +1,53 @@
 package com.example.projekt2_gruppe7.model;
 
+import java.util.List;
+
 public class WishList {
+    private Long id;
+    private Long userId;
+    private String listName;
+    private String description;
+    private List<Wish> wishes;
+
+    public WishList(Long userId, String listName, String description, List<Wish> wishes) {
+        this.userId = userId;
+        this.listName = listName;
+        this.description = description;
+        this.wishes = wishes;
+    }
+
+    public WishList() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Wish> getWishes() {
+        return wishes;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setWishes(List<Wish> wishes) {
+        this.wishes = wishes;
+    }
 }
