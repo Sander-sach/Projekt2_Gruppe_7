c568acbc2615ef327aef774cd7f8cd857c7c4441
Sander <sander.o.christensen@gmail.com>
2026-04-15 13:47:43 +0200

    WishService og WishListService nu lavet.

--- src/main/java/com/example/projekt2_gruppe7/service/WishListService.java
+++ src/main/java/com/example/projekt2_gruppe7/service/WishListService.java
@@ -1,7 +1,31 @@
 package com.example.projekt2_gruppe7.service;
 
+import com.example.projekt2_gruppe7.model.WishList;
+import com.example.projekt2_gruppe7.repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WishListService {
+
+    private final WishListRepository wishListRepository;
+
+    @Autowired
+    public WishListService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
+
+    public void createWishList(WishList wishList) {
+        wishListRepository.createWishList(wishList);
+    }
+
+    public List<WishList> getWishListsByUser(Long userId) {
+        return wishListRepository.findWishListsByUser(userId);
+    }
+
+    public WishList getWishListById(Long id) {
+        return wishListRepository.findWishListById(id);
+    }
 }
--- src/main/java/com/example/projekt2_gruppe7/service/WishService.java
+++ src/main/java/com/example/projekt2_gruppe7/service/WishService.java
@@ -1,7 +1,31 @@
 package com.example.projekt2_gruppe7.service;
 
+import com.example.projekt2_gruppe7.model.Wish;
+import com.example.projekt2_gruppe7.repository.WishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class WishService {
+
+    private final WishRepository wishRepository;
+
+    @Autowired
+    public WishService(WishRepository wishRepository) {
+        this.wishRepository = wishRepository;
+    }
+
+    public void createWish(Wish wish) {
+        wishRepository.createWish(wish);
+    }
+
+    public List<Wish> getWishesByListId(Long wishListId) {
+        return wishRepository.findWishesByListId(wishListId);
+    }
+
+    public Wish getWishById(Long id) {
+        return wishRepository.findWishById(id);
+    }
 }
