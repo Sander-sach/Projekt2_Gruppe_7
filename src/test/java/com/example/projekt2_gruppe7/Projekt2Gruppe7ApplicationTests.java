abe726cbdcc98a10b3568eb1de0f2ae884892c5e
valdemarjacobsen <vaja0001@stud.ek.dk>
2026-04-16 17:31:18 +0200

    HTML US 4 & 5

--- src/test/java/com/example/projekt2_gruppe7/Projekt2Gruppe7ApplicationTests.java
+++ src/test/java/com/example/projekt2_gruppe7/Projekt2Gruppe7ApplicationTests.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 
 class Projekt2Gruppe7ApplicationTests {
 
-    //@Test
-  //  void contextLoads() {
+    @Test
+    void contextLoads() {
         // placeholder test — passes without DB
-    //    assert true;
+        assert true;
     }
 
-//}
+}
